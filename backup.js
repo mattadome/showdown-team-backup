fad84c59c85729bdc3db3c364cdc4ade992b5239
mattadome <mattadome@gmail.com>
2020-05-21 16:32:55 +0800

    Added new filename format
    Changed filename format to YYYY-mm-dd--HH-mm-ss for easier access

--- backup.js
+++ backup.js
@@ -1,5 +1,36 @@
+var dateVar = new Date();
+var date = dateVar.getDate();
+if (date < 10){
+    date = '0' + date;
+}
 
-download('teams', stored);
+var month = dateVar.getMonth() + 1;
+if (month < 10){
+    month = '0' + month;
+}
+
+var seconds = dateVar.getSeconds();
+if (seconds < 10){
+    seconds = '0' + seconds;
+}
+
+var minutes = dateVar.getMinutes();
+if (minutes < 10){
+    minutes = '0' + minutes;
+}
+
+var hours = dateVar.getHours();
+if (hours < 10){
+    hours = '0' + hours;
+}
+
+var time = hours + '-' +  minutes + '-' + seconds;
+
+var fullDate = dateVar.getFullYear() + '-' + month + '-' + date;
+
+var teamFilename = 'Teams' + ' ' + fullDate + '--' + time;
+
+download(teamFilename, stored);
 
 function download(filename, text) {
     var element = document.createElement('a');
